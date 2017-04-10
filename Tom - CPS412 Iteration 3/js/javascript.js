688e1733037bee65d0fafcd6ec8f03279d852dfa
redpolarbear3 <bruntontom131@gmail.com>
2017-04-10 11:05:34 -0400

    Added more functionality to map

--- Tom - CPS412 Iteration 3/js/javascript.js
+++ Tom - CPS412 Iteration 3/js/javascript.js
@@ -1,4 +1,5 @@
 var map;
+var lat, lng;
 var currentPosition;
 var currentLat, currentLng;
 
@@ -30,6 +31,53 @@ function initMap() {
 		handleLocationError(false, infoWindow, map.getCenter());
 	}
 	
+	google.maps.event.addListener(map, "rightclick", function(event) {
+		lat = event.latLng.lat();
+		lng = event.latLng.lng();
+		alert("Lat=" + lat + "; Lng=" + lng);
+	});
+}
+/*
+getting custom markers from google
+   var pinColor = "FE7569";
+    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34));
+    var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new google.maps.Size(40, 37),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(12, 35));
+		
+		    var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(0,0), 
+                map: map,
+                icon: pinImage,
+                shadow: pinShadow
+            });
+https://support.google.com/fusiontables/answer/2679986
+*/
+function addMarker(Lat, Lng, title, color) {
+	var pinColor = color;
+	
+	var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+		new google.maps.Size(21,34),
+		new google.maps.Point(0,0),
+		new google.maps.Point(10,34));
+	
+	var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+		new google.maps.Size(40,37),
+		new google.maps.Point(0,0),
+		new google.maps.Point(12,35));
+	
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(Lat, Lng),
+		map: map,
+		icon: pinImage,
+		shadow: pinShadow,
+		animation: google.maps.Animation.DROP,
+		title: title
+	});
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
